4bf00cecc8ce163cfb8c2efafbaedc600139ac04
bdpriyambodo <73590204+bdpriyambodo@users.noreply.github.com>
2026-03-10 17:08:28 +0700

    add run and register command

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,6 +11,44 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
+type State struct {
+	ConfigPointer *Config
+}
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+type Commands struct {
+	Handlers map[string]func(*State, Command) error
+}
+
+func (c Commands) Run(s *State, cmd Command) error {
+	handler, exists := c.Handlers[cmd.Name]
+	if !exists {
+		return fmt.Errorf("Unknown command: %s", cmd.Name)
+	}
+	return handler(s, cmd)
+}
+
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	c.Handlers[name] = f
+}
+
+func HandlerLogin(s *State, cmd Command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("Empty handlers/argument")
+	}
+
+	(s.ConfigPointer).SetUser(cmd.Args[0])
+
+	fmt.Println("The user has been set!")
+
+	return nil
+
+}
+
 const configFileName = ".gatorconfig.json"
 
 func Read() *Config {
@@ -44,13 +82,17 @@ func Read() *Config {
 
 }
 
-func (c *Config) SetUser() {
-	c.CurrentUserName = "prymbd"
+func (c *Config) SetUser(username string) error {
+	// c.CurrentUserName = "prymbd"
+	c.CurrentUserName = username
 
 	err := write(*c)
 	if err != nil {
 		fmt.Println("Error - writing function", err)
+		return err
 	}
+
+	return nil
 }
 
 func write(cfg Config) error {
--- main.go
+++ main.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/bdpriyambodo/blog-aggregator/internal/config"
 )
 
 func main() {
-	c := config.Read()
-	c.SetUser()
+	// c := config.Read()
+	// c.SetUser("prymbd")
 
-	//check
-	cNew := config.Read()
-	fmt.Println(cNew.DbURL)
-	fmt.Println(cNew.CurrentUserName)
+	// //check
+	// cNew := config.Read()
+	// fmt.Println(cNew.DbURL)
+	// fmt.Println(cNew.CurrentUserName)
 
+	var s config.State
+	s.ConfigPointer = config.Read()
+
+	var c config.Commands
+	c.Handlers = make(map[string]func(*config.State, config.Command) error)
+
+	c.Register("login", config.HandlerLogin)
+
+	userArgs := os.Args
+	// for i, arg := range userArgs {
+	// 	fmt.Println(i, arg)
+	// }
+
+	if len(userArgs) < 2 {
+		fmt.Println("Not enough argument")
+		os.Exit(1)
+	}
+
+	if len(userArgs) < 3 {
+		fmt.Println("Username required")
+		os.Exit(1)
+	}
+
+	cmd := config.Command{
+		Name: userArgs[1],
+		Args: userArgs[2:],
+	}
+
+	c.Run(&s, cmd)
 }
